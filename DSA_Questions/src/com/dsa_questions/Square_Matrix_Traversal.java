d184414355967cf379dd8598bb18385d6fd19368
Soumit-Das <soumitdas500@gmail.com>
2024-01-24 20:47:19 +0530

    Square_Matrix_Traversal

---
+++ DSA_Questions/src/com/dsa_questions/Square_Matrix_Traversal.java
@@ -0,0 +1,39 @@
+package com.dsa_questions;
+
+import java.util.*;
+
+public class Square_Matrix_Traversal {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+        
+        int a = sc.nextInt();
+        
+        int[][] mat = new int[a][a];
+        
+        for(int i=0;i<a;i++){
+            
+            for(int j = 0;j < a;j++){
+                
+                mat[i][j] = sc.nextInt();
+                
+            }
+            
+        }
+        
+        for(int i=0;i<a;i++){
+            
+            for(int j = 0;j < a;j++){
+                
+                System.out.print((mat[i][j]+1)+" ");
+                
+            }
+            System.out.println();
+        }
+        
+    }
+		
+	}
+	
+}
